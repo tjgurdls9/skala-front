21008f15a4291b142966edaa08e9299990592655
서혁인 <tjgurdls9@gmail.com>
2026-07-20 20:24:10 +0900

    Empty GitHub repository contents

--- script/weatherAPI.js
+++
@@ -1,5 +0,0 @@
-export async function getWeatherData(lat, lon) {
-    var url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m`;
-    var response = await fetch(url);
-    return await response.json();
-}
